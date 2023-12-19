14edc907540b90f5c09ae86d7a5114d032fa705d
2sandeep7 <ssandeep8696@gmail.com>
2023-12-19 15:00:55 -0500

    added navigation sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,15 +1,40 @@
 "use client"
 
-import React from 'react'
+import { cn } from '@/lib/utils';
+import { ChevronLeft } from 'lucide-react'
+import { usePathname } from 'next/navigation';
+import React, { ElementRef, useRef,useState } from 'react'
 
+import { useMediaQuery } from 'usehooks-ts';
 
 const Navigation = () => {
+const pathName=usePathname();
+const isMobile=useMediaQuery("(max-width:768px)");
+
+const isResizingRef=useRef(false);
+const sidebarRef = useRef<ElementRef<"aside">>(null);
+const navbarRef = useRef<ElementRef<"div">>(null);
+const [isResetting,setIsResetting] = useState(false);
+const [isCollapsed,setIsCollapsed] = useState(isMobile);
+
+
   return (
     <>
     <aside
-    className='
-    group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]'
+    ref={sidebarRef}
+    className={cn("group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+    isResetting && "transition-all ease-in-out duration-300",
+    isMobile && "w-0"
+    )}
     >
+      <div 
+      role='button'
+      className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+      isMobile && " opacity-100"
+      )}
+      >
+        <ChevronLeft className='h-6 w-6 '/>
+      </div>
 
         <div>
             <p>Action items</p>
